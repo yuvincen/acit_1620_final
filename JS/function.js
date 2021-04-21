248b80e354758c08edcc4139ea9409bd37ee9e30
Vincent Yu <vincentyu@Macbook-Pro-3.hitronhub.home>
2021-04-21 14:26:18 -0700

    adding in js code

--- JS/function.js
+++ JS/function.js
@@ -3,3 +3,10 @@ var allImages = document.getElementsByTagName("Album 1", "Album 2", "Album 3", "
 for(var i = 0); i < allImages.length ; i++) {
     allImages[i].src="/Images/"
 }
+
+const albums = [
+    { name: "Album 1", price: 12.99, inCart: false }, 
+    { name: "Album 2", price: 14.99, inCart: false }, 
+    { name: "Album 3", price: 9.99, inCart: false }, 
+    { name: "Album 4", price: 19.99, inCart: false }
+  ];
